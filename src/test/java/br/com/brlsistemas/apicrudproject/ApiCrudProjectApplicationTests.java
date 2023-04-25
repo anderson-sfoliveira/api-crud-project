3ba60d92bbae6abdea17094d899e3fa876b21a32
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2023-04-25 18:40:50 -0300

    first commit

---
+++ src/test/java/br/com/brlsistemas/apicrudproject/ApiCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brlsistemas.apicrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
